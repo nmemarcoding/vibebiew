bb4a9ce15d85070a2ffbc5c9cfc30af0638ad701
nmemarcoding <nmemarcoding@gmail.com>
2023-08-23 13:52:14 -0700

    express app with mongodb created

---
+++ api/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const{mongoose} = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+// Connect to DB
+
+
+mongoose
+    .connect("mongodb+srv://nmemarcoding:Nima1377@cluster0.va0yfxf.mongodb.net/")
+    .then(() => console.log("DB Connection Successfull!"))
+    .catch((err) => {
+        console.log(err);
+})
+
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+}
+);
