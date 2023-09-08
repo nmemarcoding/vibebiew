99f34336f6c3945e1c99b34acd7545a596cfe2ab
nmemarcoding <nmemarcoding@gmail.com>
2023-09-08 12:14:31 -0700

    post fetching from server and showing on home page

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -2,8 +2,24 @@ import React from 'react'
 import Navbar from '../../componets/Navbar/Navbar'
 import NewPost from '../../componets/NewPost/NewPost'
 import Post from '../../componets/Post/Post'
+import store from '../../store.js'
+import {publicRequest} from '../../hooks/requestMethods.js'
 export default function 
 () {
+    const userInfo = store.getState().userInfo
+    // fettching posts from server
+    const [posts, setPosts] = React.useState([])
+    React.useEffect(() => {
+        publicRequest().get('/post/timeline/' + userInfo._id)
+            .then(res => {
+                setPosts(res.data)
+                console.log(res.data)
+            }).catch(err => {
+                console.log(err)
+            }
+            )
+    }, [])
+    
     const samplePosts = [
         {
           username: "JohnDoe",
@@ -33,14 +49,14 @@ export default function
         <div className="App p-4 space-y-4">
             <Navbar />
           <NewPost />
-          {samplePosts.map((post, index) => (
+          {posts?.map((post, index) => (
         <Post 
           key={index}
-          username={post.username}
-          timestamp={post.timestamp}
-          content={post.content}
-          likes={post.likes}
-          commentList={post.commentList}
+          username={`${post?.userId?.firstName} ${post?.userId?.lastName}`}
+          timestamp={new Date(post.createdAt).toLocaleString('en-US', { year: 'numeric', month: 'short', day: 'numeric', hour: '2-digit', minute: '2-digit' }).replace(",", "")}
+          content={post.desc}
+          likes={post.likes.length}
+          commentList={post.comments}
         />
       ))}
         </div>
