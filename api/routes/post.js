36fc935322427fc47145a8f564e9192a8679159a
nmemarcoding <nmemarcoding@gmail.com>
2023-09-15 11:46:21 -0700

    populating comment and user id inside the comment

--- api/routes/post.js
+++ api/routes/post.js
@@ -27,44 +27,30 @@ router.post("/", async(req, res) => {
 });
 
 // get rout to get post of a user and all of their followings
-router.get("/timeline/:userId", async(req, res) => {
-    try {
-        const currentUser = await User.findById(req.params.userId);
-        const userPosts = await Post.find({ userId: req.params.userId }).populate({ path: "userId", select: "firstName lastName" });
-        // map over all user posts and check if commment exist populate the comment with the user info
-        const userPostsWithComments = await Promise.all(
-            userPosts.map(async(post) => {
-                const comments = await Promise.all(
-                    post.comments.map(async(comment) => {
-                        const commentWithUser = await comment.populate({ path: "userId", select: "firstName lastName" }).execPopulate();
-                        return commentWithUser;
-                    })
-                );
-                return { ...post._doc, comments };
-            })
-        );
-        const friendPosts = await Promise.all(
-            currentUser.following.map((friendId) => {
-                return Post.find({ userId: friendId });
-            })
-        );
-        // map over all friend posts and check if commment exist populate the comment with the user info
-        const friendPostsWithComments = await Promise.all(
-            friendPosts.map(async(post) => {
-                const comments = await Promise.all(
-                    post.comments.map(async(comment) => {
-                        const commentWithUser = await comment.populate({ path: "userId", select: "firstName lastName" }).execPopulate();
-                        return commentWithUser;
-                    })
-                );
-                return { ...post._doc, comments };
-            })
-        );
-        res.status(200).json(userPostsWithComments.concat(...friendPostsWithComments));
+router.get("/timeline/:userId", async (req, res) => {
+    // check if userId is valid for mongoose
+    if (!mongoose.Types.ObjectId.isValid(req.params.userId)) {
+        return res.status(400).json({ error: "User id is not valid" });
+    }
+    // find all of the user posts base on user id and populate userId without password
+    const post = await Post.find({
+        userId: req.params.userId
+    }).populate({
+        path: "userId",
+        select: "-password"
+    }).populate({
+        path: "comments",
+        populate: {
+            path: "userId",
+            select: "-password"
+        }
+    });
 
-        
+    try {
+        res.status(200).json(post);
     } catch (err) {
         res.status(500).json(err);
+        console.log(err);
     }
 });
 
